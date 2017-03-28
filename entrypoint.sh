4e8e57937cc49ae4fa7272b025f412390ce82021
malaohu <re_ming.ma@hotmail.com>
2017-03-28 15:12:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
-#ping www.baidu.com -c 5
 
-echo "- yes it start~"
-#/usr/bin/python /src/ssr/shadowsocks/server.py "$@"
+#start net-speeder
+nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
+
+#start ssd
+/usr/sbin/sshd -D
+
+
+nohup python /src/ssr/shadowsocks/server.py "$@" >/dev/null 2>&1 &
