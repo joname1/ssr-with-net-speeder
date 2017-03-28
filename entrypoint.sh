96d51376e6697b4ceb726b737bc74ee63b27172f
malaohu <re_ming.ma@hotmail.com>
2017-03-28 16:21:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
 #start ssd
 /usr/sbin/sshd -D
 
-echo "-- Start Info : $@"
+echo "-- Start Info : $@" >> /src/log.log
+echo "-- Start Info : $1" >> /src/log.log
 
-nohup python /src/ssr/shadowsocks/server.py "$@" >/dev/null 2>&1 &
+nohup python /src/ssr/shadowsocks/server.py $1 >/dev/null 2>&1 &
