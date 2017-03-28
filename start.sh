dd77ee65711a611ff005a0a300a3561d7081e3e9
malaohu <re_ming.ma@hotmail.com>
2017-03-28 14:00:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,13 @@ nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
 
 #start ssd
 /usr/sbin/sshd -D
+
+#start ss
+python /shadowsocks/server.py \
+    -s 0.0.0.0 \
+    -p $SERVER_PORT \
+    -k $PASSWORD    \
+    -m $METHOD      \
+    -O $PROTOCOL    \
+    -o $OBFS        \
+    -t 300
